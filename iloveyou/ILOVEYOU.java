f225c2abad0c5cfc4110a7c5eeeef75784b2b55c
25017630 <mualusimugeri@gmail.com>
2025-06-08 03:37:35 +0200

    first commit

---
+++ iloveyou/ILOVEYOU.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package iloveyou;
+
+/**
+ *
+ * @author MUALUSI MUGERI
+ */
+public class ILOVEYOU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i=10;i<=10;i--){
+        
+     if(i%2==1)
+         System.out.println(i);
+  
+            
+                
+            } 
+    }
+    
+}
+    
+  
+        
